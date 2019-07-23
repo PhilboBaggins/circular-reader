e24f5757de784ce3b8e0dd60ddd2e81a8b9f0f3b
Phil <PhilboBaggins@users.noreply.github.com>
2019-07-24 02:54:23 +1000

    Basic working implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,42 @@
+#[allow(dead_code)]
+struct CircularReader {
+    pos: usize,
+    buf: Vec<usize>,
+}
+
+impl CircularReader {
+    #[allow(dead_code)]
+    fn new(buf: Vec<usize>) -> CircularReader {
+        CircularReader { pos: 0, buf: buf }
+    }
+}
+
+impl Iterator for CircularReader {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ret = self.buf[self.pos];
+        self.pos += 1;
+        if self.pos >= self.buf.len() {
+            self.pos = 0;
+        }
+        Some(ret)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let vec = vec!(1, 2, 3);
+        let mut cr = CircularReader::new(vec);
+        assert_eq!(cr.next(), Some(1));
+        assert_eq!(cr.next(), Some(2));
+        assert_eq!(cr.next(), Some(3));
+        assert_eq!(cr.next(), Some(1));
+        assert_eq!(cr.next(), Some(2));
+        assert_eq!(cr.next(), Some(3));
     }
 }
